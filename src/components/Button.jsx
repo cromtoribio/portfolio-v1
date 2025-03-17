2f1c3898c1722abc57bc09cc17e09f016066c1a9
Chris Toribio <44413949+cromtoribio@users.noreply.github.com>
2025-03-17 14:46:24 -0700

    Changed buttons to open in same tab

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,6 +2,6 @@ import './Button.css'
 
 export default function Button({ type, link, children}) {
     return <>
-        <a className={type} href={link} target="_blank" rel="noopener noreferrer">{children}</a>
+        <a className={type} href={link} rel="noopener noreferrer">{children}</a>
     </>
 }
